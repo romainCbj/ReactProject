736d27fe309e62ef77606e22c9290d010a03447c
Romain Desruelle <romain.desruelle@elosi.lan>
2024-07-16 11:51:18 +0200

    Correction faute de frappe

--- src/ToDo.js
+++ src/ToDo.js
@@ -6,12 +6,11 @@ import ReactDOM from 'react-dom/client';
 function ToDo() {
     const [taskList, setTaskList] = useState([]);
     function confirmed(e) {
-        console.log(e);
         setTaskList(taskList.filter(a=>a.name !== e.name));
     }
     
     function submit(e) {
-        if (e.key === 'Enter') {
+        if (e.key === 'Enter' && e.target.value !=="") {
             setTaskList([
                 ...taskList,
                 {  name: e.target.value, date :  Date() }
@@ -20,26 +19,19 @@ function ToDo() {
        }
 
     return (
-    <div>
-
-        <input type="Text" placeholder="Saisissez une tache" onKeyDown={submit}></input>
-        <div>{taskList.length}</div>
-        <table>
-            <tbody>
-
-            {taskList.length>0 ? taskList.map((task => 
-            <tr >
-                <td>
-                {task.name}
-                </td>
-           <td>{task.date}</td>
-           <td><button onClick={() => confirmed(task)}>OK</button></td>
-            </tr>))  : null}
-
-            </tbody>
-        </table>
-
-    </div>
+        <div>
+            <input type="Text" placeholder="Saisissez une tache" onKeyDown={submit}></input>
+            <table>
+                <tbody>
+                    {taskList.length>0 ? taskList.map((task => 
+                        <tr >
+                            <td>{task.name}</td>
+                            <td><button onClick={() => confirmed(task)}>OK</button></td>
+                        </tr>))  
+                    : null}
+                </tbody>
+            </table>
+        </div>
     );
 }; 
 
